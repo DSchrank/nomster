c22b6a94f5344b03bb4a5fefc64203de97da7cc8
Dane Schrank <danerrr@mail.com>
2019-05-30 01:55:53 +0000

    made the update page and edited the development.rb file to prevent having to restart server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Possible fix to restarting server every controller change
+  config.reload_classes_only_on_change = false
+
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
