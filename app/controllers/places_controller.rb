82311787aa107e72e52d1658f54c2fd2726d7022
Dane Schrank <danerrr@mail.com>
2019-05-30 02:43:21 +0000

    secured the edit and update feature to be sure it was only current user doing so

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,33 +1,53 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+
 
   def index
     @places = Place.all.page(params[:page]).per_page(2)
   end
 
 
+
   def new
     @place = Place.new
   end
+
+
 
   def create
     current_user.places.create(place_params)
     redirect_to root_path
   end
 
+
+
   def show
     @place = Place.find(params[:id])
   end
 
+
+
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
+
+
 
   def update
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
     @place.update_attributes(place_params)
     redirect_to root_path
   end
+
+
 
   def destroy
    @place = Place.find(params[:id])
