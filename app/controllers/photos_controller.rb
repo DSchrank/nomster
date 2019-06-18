9fd67d6a40656f07b030f1a90be6e93b877cfd43
Dane Schrank <danerrr@mail.com>
2019-06-18 00:05:17 +0000

    half way done with the places photos. installing gem next

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,14 @@
 class PhotosController < ApplicationController
+    before_action :authenticate_user!
 
+    
   def new
     @photo = Photo.new
   end
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(params[:photo_params])
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
